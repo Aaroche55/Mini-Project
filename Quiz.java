930eeea8a69102c85a179fb084be9587b1e06f9f
Ushen Wijayaratne <108585387+its-ushen@users.noreply.github.com>
2023-10-10 19:48:00 +0100

    Create Quiz.java

---
+++ Quiz.java
@@ -0,0 +1,62 @@
+public class Quiz {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        // Creating arrays
+
+        // create array of questions and options
+            //format - one string for each question 
+                // initial question
+                    // (a) option 1
+                    // (b) option 2
+                    // (c) option 3
+
+                // new question 
+                    // (a) option 1
+                    // (b) option 2
+                    // (c) option 3
+
+
+        // create array of answers - same index
+            // format 
+                // letter
+
+        // User Interation
+
+        // ask username 
+        // ask password
+
+        // Print Welcome 
+        // Learn / Test
+            // if statement - if t- call test 
+
+            // return score in the end
+        }
+        
+        public static void learn(String questions[], String answers[]) {
+            // for loop 0 - 17 
+            // print the q and answer 
+            
+            // layout
+            // Q [i] - adfsdfsd
+            // A [i] - adhsdfdsdfsd
+            
+            // q [2]
+
+    }
+    
+    public static int test(String questions[], String answers[]) {
+        // create variable for score
+        
+        // create for loop 0-5 (qs)
+        // print  qs and options
+        
+        // ask which option chosen
+        // if statement
+        // if option chosen = correct answer
+        // add 1 to tally
+        
+        
+        //return variable 
+        return 0;
+    }
+}
