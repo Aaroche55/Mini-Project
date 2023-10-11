0f4d9c1687ec779158d86410e65346209ead2ba2
Aaroche <aaroche55@gmail.com>
2023-10-11 16:22:15 +0100

    Iteration1

--- Quiz.java
+++ Quiz.java
@@ -1,3 +1,4 @@
+
 public class Quiz {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -35,7 +36,9 @@ public class Quiz {
         public static void learn(String questions[], String answers[]) {
             // for loop 0 - 17 
             // print the q and answer 
-            
+            for (i = 0; i < questions.length; i++) {
+            	System.out.println(questions[i] + answers[i]);
+            }
             // layout
             // Q [i] - adfsdfsd
             // A [i] - adhsdfdsdfsd
@@ -46,9 +49,27 @@ public class Quiz {
     
     public static int test(String questions[], String answers[]) {
         // create variable for score
-        
+        score = 0
         // create for loop 0-5 (qs)
         // print  qs and options
+        for(i = 0; i < 6;i++) {
+        	System.out.println(questions[i]);
+        	char ans = scanner.nextChar();
+        	if(ans = answers[i]) {
+        		score++;
+        	}	
+        }
+        //if(score<=2) {
+        	//System.out.println("Sucks to suck. \n Score: " + score);
+        //}
+        //if((score > 2) && (score <= 4)) {
+        	//System.out.println("Meh. \n Score: " + score);
+        //}
+        //if((score == 5)) {
+        	//System.out.println("So Close. \n Score: " + score);
+        //}
+        //if((score == 6)) {
+        	//System.out.println("Perfect. \n Score: " + score);
         
         // ask which option chosen
         // if statement
@@ -57,6 +78,6 @@ public class Quiz {
         
         
         //return variable 
-        return 0;
+        return score;
     }
 }
