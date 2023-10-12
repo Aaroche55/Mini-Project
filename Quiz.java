42e01f36f4f159e23ba5e34a8e5d7da0bbc98443
its-ushen <ushen05@gmail.com>
2023-10-12 11:00:46 +0100

    first iteration

--- Quiz.java
+++ Quiz.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Quiz {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         // Creating arrays
-        // create array of questions and options
-        // format - one string for each question
-        // initial question
-        // (a) option 1
-        // (b) option 2
-        // (c) option 3
+                 // create array of questions and options
+            //format - one string for each question 
+                // initial question
+                    // (a) option 1
+                    // (b) option 2 
+                    // (c) option 3
 
-        // new question
-        // (a) option 1
-        // (b) option 2
-        // (c) option 3
+                // new question 
+                    // (a) option 1
+                    // (b) option 2
+                    // (c) option 3
         String[] questions = new String[6];
 
         questions[0] = "Which of the following is a prime number\n  a. 4\n  b. 9\n  c. 13";
@@ -23,40 +22,61 @@ public class Quiz {
         questions[3] = "In which part of the computer is data processed?\n  a. Hard Drive\n  b. RAM\n  c. CPU";
         questions[4] = "Which of the following is considered a high-level programming language?\n  a. Machine Language\n  b. Python\n  c. Assembly Language";
         questions[5] = "What does the acronym 'URL' stand for?\n  a. Universal Resource Locator\n  b. Unified Retrieval Logic\n  c. Uniform Resource Locator";
-
+        
         // create array of answers - same index
-        // format
-        // letter
+            // format 
+                // letter
 
         char[] answers = new char[6];
-        answers[0] = 'c';
-        answers[1] = 'c';
-        answers[2] = 'b';
-        answers[3] = 'c';
-        answers[4] = 'b';
-        answers[5] = 'c';
+        answers[0]= 'c';
+        answers[1]= 'c';
+        answers[2]= 'b';
+        answers[3]= 'c';
+        answers[4]= 'b';
+        answers[5]= 'c';
 
         // User Interation
         Scanner scanner = new Scanner(System.in);
 
-        // ask username
+        // ask username 
         System.out.print("Enter your username: ");
         String username = scanner.next();
         // ask password
         System.out.print("Enter your password: ");
         String password = scanner.next();
 
-        // Print Welcome
+        // Print Welcome 
         // Learn / Test
-        // if statement - if t- call test
+            // if statement - if t- call test 
 
             // return score in the end
+
+        if (username instanceof String || password instanceof String) {
+            System.out.println("\nWelcome!\n");
+            System.out.print("Do you want to learn or test? ");
+            
+            String mode = scanner.next();
+
+            if (mode.equals("learn")) {
+                learn(questions, answers);
+                scanner.close();
+                return;
+            }
+
+            else {
+                int score = test(questions, answers);
+                // print feedback 
+                System.out.println("Well done you got a score of " + score + "/6");
+            }
+        }
+
+        scanner.close();
     }
         
-    public static void learn(String questions[], String answers[]) {
+    public static void learn(String questions[], char answers[]) {
         // for loop 0 - 17 
         // print the q and answer 
-        
+
         // layout
         // Q [i] - adfsdfsd
         // A [i] - adhsdfdsdfsd
