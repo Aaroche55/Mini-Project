ca99afe0d9a8b169f565be503c9ffa6a082a86fe
its-ushen <ushen05@gmail.com>
2023-10-13 12:27:53 +0100

    if username already exist

--- EPIC/src/Authentication.java
+++ EPIC/src/Authentication.java
@@ -10,27 +10,37 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
 public class Authentication {
-    public static void main(String[] args) throws Exception {
-        signup("Fahim", "Rahman");
-        System.out.println(check("Fahim", "Rahman")); // Output: true
-    }
-
-    public static void signup(String username, String password) throws Exception {
+    public static boolean signup(String username, String password) throws Exception {
+        // Check if the username already exists
+        try (BufferedReader reader = new BufferedReader(new FileReader("users.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts[0].equals(username)) {
+                    System.out.println("Username already exists. Please choose a different username.");
+                    return false;
+                }
+            }
+        }
+    
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
         random.nextBytes(salt);
-
+    
         KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-
+    
         byte[] hash = factory.generateSecret(spec).getEncoded();
-
+    
         try (FileWriter writer = new FileWriter("users.txt", true);
              BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
             bufferedWriter.write(username + "," + Base64.getEncoder().encodeToString(hash) + ","
                     + Base64.getEncoder().encodeToString(salt));
             bufferedWriter.newLine();
         }
+    
+        System.out.println("User registered successfully!");
+        return true;
     }
 
     public static boolean check(String username, String password) throws Exception {
