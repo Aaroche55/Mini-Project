93b815c989e314aa389bc15dcf44ba8671ee300d
its-ushen <ushen05@gmail.com>
2023-10-31 18:35:41 +0000

    Final Push (hopefully)

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,25 +1,9 @@
 package com.example;
 
-import com.example.quiz.QuizLogic;
+import com.example.gui.loginGUI;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        // for (int i = 0; i < 8 ; i++)
-        // {
-        //     System.out.println(MeanCalculator.get_mean("JohnDoe")[i]);
-        //     System.out.println(StandardDeviationCalculator.get_standardDeviation("JohnDoe")[i]);
-        //     System.out.println(MedianCalculator.get_median("JohnDoe")[i]);
-        //     System.out.println();
-        // }
-
-        for (int i = 0; i < 6; i++){
-            System.out.println(QuizLogic.get_questions().get(i));
-            System.out.println(QuizLogic.get_options().get(i));
-            System.out.println(QuizLogic.get_answers().get(i));
-            System.out.println();
-        }
-
-        
+        loginGUI.main(args);
     }
 }
